0f5d8e94ea6126107d5d875c564701d188511798
raghusodani <201951118@iiitvadodara.ac.in>
2021-04-15 21:27:12 +0530

    routes file added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./components/homepage/homepage.js";
 import Login from "./components/login/login.js";
 import RegisterPage from "./components/register/RegisterPage.js"
 function App() {
   return (
-    <div>
-       
-      <RegisterPage />
-      <Homepage />
-      <Login/>
-    </div>
+    <BrowserRouter >
+    <Switch>
+      <Route exact path="#" component={Homepage} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={RegisterPage} />
+      <Route exact path='/' component={Homepage} />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,13 +1,39 @@
-import React from "react";
-
+import React , {useState} from "react";
+import { Redirect, useHistory } from 'react-router-dom';
 function Homepage() {
+  const history = useHistory();
+  const routeChange1 = () =>{ 
+    console.log("yes clicked");
+    let path = '/login'; 
+    history.push(path);
+  }
+  const routeChange2 = () =>{ 
+    console.log("yes clicked");
+    let path = '/register'; 
+    history.push(path);
+  }
+  const [isOpen, updateIsOpen] = useState(true);
+  if(!isOpen){
+    console.log("yes clicked");
+    return <Redirect to="/test" />
+  }
+  // eslint-disable-next-line
+function handleClick() {
+  updateIsOpen(!isOpen);
+}
   return (
     <div>
     <h1>Home</h1>
-      <button> donor </button>
-      <button> recipient </button>
-      <button> blood bank </button>
-      <button> Hospital </button>
+      <button
+      onClick={routeChange1}
+      > Login 
+      </button>
+
+      <button
+      onClick={routeChange2}
+      > Register 
+      </button>
+
     </div>
    );
 }
