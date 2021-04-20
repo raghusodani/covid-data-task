73c2e2b57f9f655065951b67c5b76902d04b8e43
raghusodani <201951118@iiitvadodara.ac.in>
2021-04-20 20:15:00 +0530

    validation added

--- src/components/register/RegisterPage.js
+++ src/components/register/RegisterPage.js
@@ -1,51 +1,146 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Axios from 'axios';
 import './regStyle.css';
 
 function RegisterPage() {
-  const [name, setName] = useState('');
-  const [bloodGroup, setBloodGroup] = useState('');
-  const [nameList, setNameList] = useState([]);
-
-  useEffect(() => {
-    Axios.get('http://localhost:3001/api/get').then((response) => {
-      setNameList(response.data);
-    });
+  const [userReg,setUserReg] = useState({
+    name:"",
+    email:"",
+    address:"",
+    phone:"",
+    bloodGroup:"",
+    password:""
   });
-  const submitForm = () => {
-    console.log('laaya bapu');
-    Axios.post('http://localhost:3001/api/insert', {
-      name: name,
-      bloodGroup: bloodGroup,
-    }).then(() => {
-      alert('successfully inserted');
-    });
+  let flag = 1;
+  
+ 
+  
+  function validate(values){
+    let error = {};
+     //name
+    if(!values.name.trim()){
+      error.name = "Name required";
+      flag=0;
+    }
+
+     //email
+    if (!values.email) {
+      error.email = 'Email required';
+      flag=0;
+      
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      error.email = 'Email address is invalid';
+      flag=0;
+    }
+    //address
+    if(!values.address){
+      error.address = 'Address is required';
+      flag=0;
+    }
+
+    //bloodGroup
+    const validBloodGroupRegex = /^(A|B|AB|O)[+-]$/i; 
+    if(!values.bloodGroup){
+    error.bloodGroup = "Blood Group is required";
+    //console.log('aayabg');
+   // console.log(isValid)
+   flag=0;
+    }
+    if(!validBloodGroupRegex.test(values.bloodGroup)){
+    error.bloodGroup = 'Enter Valid Blood group';
+    flag=0;}
+    //phone
+    var pattern = new RegExp(/^[0-9\b]+$/);
+    if(!values.phone){
+    error.phone = 'Phone Number is required';
+    flag=0;}
+    if (!pattern.test(values.phone)) {
+      error.phone = "Please enter only number without country code";
+      flag=0;
+    }else if(values.phone.length !== 10){
+      error.phone = "Please enter valid phone number.";
+      flag=0;
+    }
+  
+    //password
+    if (!values.password) {
+      error.password = 'Password is required';
+      flag=0;
+    } else if (values.password.length < 6) {
+      error.password = 'Password needs to be 6 characters or more';
+      flag=0;
+    }
+    return error;
+  }
+  const[errors,setErrors] = useState({});
+  const [records,setRecords] = useState([]);
+
+  const handleInput = (e) =>{
+    const nameOfInput = e.target.name;
+    const value = e.target.value;
+    
+    setUserReg({...userReg , [nameOfInput] : value});
+  }
+  const submitForm= (e) => {
+    e.preventDefault();
+    const newUser = {
+      ...userReg,id : new Date().getTime().toString()
+    }
+    
+    setRecords([ ...records,newUser]);
+    setErrors(validate(userReg));
+    
+    if(flag===1){
+      console.log("post gayi");
+      Axios.post('http://localhost:3001/api/insert', {
+        name: userReg.name,
+        email: userReg.email,
+        address:userReg.address,
+        phone : userReg.phone,
+        bloodGroup: userReg.bloodGroup,
+        password : userReg.password
+      }).then( () => {
+        console.log("success");
+      });
+    };
+    e.target.reset();
+    
   };
+
   return (
     <div class='register-form'>
-      <form class='form'>
+      <form class='form' onSubmit={submitForm}>
         <h1> Register </h1>
         <input
           type='text'
           name='name'
           placeholder='Name'
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
+          autoComplete="off"
+          onChange= {handleInput}
         ></input>
+        {errors.name && <p>{errors.name}</p>}
         <input
           type='text'
           name='email'
           placeholder='Email'
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
+          autoComplete="off"
+          onChange={handleInput}
         ></input>
-        <input type='text' name='address' placeholder='Address'></input>
-        <input type='text' name='phone' placeholder='Phone Number'></input>
-        <input type='text' name='bloodGroup' placeholder='Blood group'></input>
-        <input type='password' name='password' placeholder='Password' />
-        <button onClick={submitForm}> Submit </button>
+        {errors.email && <p>{errors.email}</p>}
+        <input type='text' name='address' placeholder='Address' autoComplete="off"
+        onChange= {handleInput} ></input>
+        {errors.address && <p>{errors.address}</p>}
+        <input type='text' name='phone' placeholder='Phone Number'  autoComplete="off"
+        onChange={handleInput}></input>
+        {errors.phone && <p>{errors.phone}</p>}
+        <input type='text' name='bloodGroup' placeholder='Blood group' autoComplete="off"
+        onChange= {handleInput} ></input>
+        {errors.bloodGroup && <p>{errors.bloodGroup}</p>}
+        <input  type='password' name='password' placeholder='Password' autoComplete="off"
+        onChange= {handleInput} />
+        {errors.password && <p>{errors.password}</p>}
+        <button type="submit" >Submit</button>
         {/* {nameList.map((val) => {
           return (<h1> Name :{val.name} </h1>);
         })} */}
