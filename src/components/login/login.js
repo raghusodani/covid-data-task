b38be28a22b0faf9dd5f066bec4a776fc9f2f958
raghusodani <201951118@iiitvadodara.ac.in>
2021-04-23 19:17:09 +0530

    login improved

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -28,7 +28,6 @@ function Login() {
   return (
     <div class='login-page form'>
       <h1> Login </h1>
-<<<<<<< HEAD
       <form class='login-form' onSubmit={ (e) => {e.preventDefault();}}>
         <input type='text' name='email' placeholder='email' 
         onChange = { (e) => {
@@ -42,18 +41,7 @@ function Login() {
         <button onClick={handleClick}>Login</button>
         <p>{loginStatus}</p>
 =======
-      <form class='login-form'>
-        <input type='text' name='name' placeholder='Name*' />
-        {/* TODO: add validation logic */}
-        <p class='valid'>username is required</p>
-        <input type='password' name='password' placeholder='Password*' />
-        <p class='valid'>password is required</p>
-        <p id='required-text'>* Required Fields</p>
-        <button>login</button>
-        {/* {nameList.map((val) => {
-          return (<h1> Name :{val.name} </h1>);
-        })} */}
->>>>>>> c50b9d63e023b462a3e77cc90f8a173282b30cd8
+      
         <p class='message'>
           Not registered? <a href='/register'>Create an account</a>
         </p>
