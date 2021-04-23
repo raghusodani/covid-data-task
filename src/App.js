95f5f30d4e1513de442180d6120237c2c40a24ad
raghusodani <201951118@iiitvadodara.ac.in>
2021-04-23 19:05:46 +0530

    Login added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import Homepage from "./components/homepage/homepage.js";
 import Login from "./components/login/login.js";
 import RegisterPage from "./components/register/RegisterPage.js"
+import Dash from "./components/dashboard/dash"
 function App() {
   return (
     <BrowserRouter >
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="#" component={Homepage} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={RegisterPage} />
+      <Route exact path='/dashboard' component={Dash} />
       <Route exact path='/' component={Homepage} />
     </Switch>
   </BrowserRouter>
---
+++ src/components/dashboard/dash.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+function Dash() {
+    return(
+        <h1>hmm</h1>
+    );
+}
+
+export default Dash;
